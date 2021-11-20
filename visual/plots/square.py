a1b8b5e4556d8f4a7204b6cdc4a599bdd9b30d28
Princess Janet <5ayorj23@solent.ac.uk>
2021-11-21 11:15:12 +1200

    Created a program to display squares using a line plot.

---
+++ visual/plots/square.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+
+def small():
+    x = [3, 3, 4, 4, 3]
+    y = [3, 4, 4, 3, 3]
+    plt.plot(x, y, "ro:")
+
+
+def medium():
+    x = [2, 2, 5, 5, 2]
+    y = [2, 5, 5, 2, 2]
+    plt.plot(x, y, "gs--")
+
+
+def large():
+    x = [1, 1, 6, 6, 1]
+    y = [1, 6, 6, 1, 1]
+    plt.plot(x, y, "bp-")
+
+
+def run():
+    small()
+    medium()
+    large()
+    plt.show()
+
+
+run()
