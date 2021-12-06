db308788038c95165f7f57ec5d835153299c2e6c
Princess Janet <5ayorj23@solent.ac.uk>
2021-12-07 10:50:08 +1200

    created a function to display menu of option

--- COM 728 assessment/tui.py
+++ COM 728 assessment/tui.py
@@ -110,8 +110,31 @@ def menu(variant=0):
 
     :return: nothing if invalid selection otherwise an integer for a valid selection
     """
-    # TODO: Your code here
-    pass
+    if variant == 0:
+        print("Please select one of the following option:")
+        print("""'[1] Process Data', '[2] Query Database', '[3] Visualise Data' and '[4] Exit'""")
+        option = int(input("Your selection:"))
+        return option
+    if variant == 1:
+        print(""" '[1] Record by Serial Number', '[2] Records by Observation Date', '[3] Group Records by Country/Region,
+            '[4] Summarise Records'""")
+        option = int(input("Your selection:"))
+        return option
+    if variant == 2:
+        print(""" '[1] Setup database',
+        '[2] Retrieve all countries in alphabetical order from the database',
+        '[3] Retrieve confirmed cases, deaths and recoveries for an observation from the database',
+        '[4] Retrieve top 5 countries for confirmed cases from the database from the database',
+        '[5] Retrieve top 5 countries for deaths for specific observation dates form the database'""")
+        option = int(input("Your selection:"))
+        return option
+    if variant == 3:
+        print(""" '[1] Country/Region Pie Chart', '[2] Observations Chart', '[3] Animated Summary'""")
+        option = int(input("Your selection:"))
+        return option
+
+
+menu(3)
 
 
 def total_records(num_records):
