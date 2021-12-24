b5fed374cd4fcb6cf542154f944c7bd26e01be3c
princessjanet <5ayorj23@solent.ac.uk>
2021-12-24 09:01:34 +0000

    task 10 to 14 is done

--- COM 728 assessment/main.py
+++ COM 728 assessment/main.py
@@ -12,17 +12,22 @@ Note:   any user input/output should be done using the appropriate functions in 
 
 # Task 10: Import required modules
 # TODO: Your code here
+import tui
+import process
+import database
+
 
 # Task 11: Create an empty list named 'covid_records'.
 # This will be used to store the data read from the source data file.
 # TODO: Your code here
+covid_records = []
 
 
 def run():
     # Task 12: Call the function welcome of the module 'tui'.
     # This will display our welcome message when the program is executed.
     # TODO: Your code here
-
+tui.welcome()
     # Task 13: Load the data.
     # - Use the appropriate function in the module 'tui' to display a message to indicate that the data loading
     # operation has started.
@@ -31,12 +36,28 @@ def run():
     # - Use the appropriate functions in the module 'tui' to display a message to indicate how many records have
     # been loaded and that the data loading operation has completed.
     # TODO: Your code here
+tui.progress("data loading",0)
+file_path = "covid_19_data.csv"
+covid_records = []
+try:
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        headings = next(csv_reader)
 
+        for line in csv_reader:
+            covid_records.append(line)
+except IOError:
+print("cannot load file")
+tui.total_records()
+tui.progress("data loading opearation",100)
     while True:
         # Task 14: Using the appropriate function in the module 'tui', display a menu of options
         # for the different operations that can be performed on the data (menu variant 0).
         # Assign the selected option to a suitable local variable
         # TODO: Your code here
+    tui.menu()
+    selected_option = 0
+
 
         # Task 15: Check if the user selected the option for processing data.  If so, then do the following:
         # - Use the appropriate function in the module tui to display a message to indicate that the data processing
